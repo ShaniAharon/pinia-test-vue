c177bac00e43ddaa71f78ef28a39f1b0da065448
shforeal <rivena14798@gmail.com>
2022-07-08 15:40:10 +0300

    review: clean

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
+  <header class="container-center">
+    <nav class="">
+      <RouterLink class="fg-2" to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
       <RouterLink to="/todo">Todos</RouterLink>
     </nav>
