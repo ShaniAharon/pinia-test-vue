7d5a7a9a510a9434daf041176424eb1d4a6a68db
shforeal <rivena14798@gmail.com>
2022-07-08 14:30:27 +0300

    init-proj

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  // import TheWelcome from '@/components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <h1>HEEe</h1>
+  </main>
+</template>
