1cd555561381a6648949fb3ac0597195c229557b
shforeal <rivena14798@gmail.com>
2022-07-09 12:44:10 +0300

    feat: add preview

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
   import {Todo} from '@/types/todo'
   import {onMounted} from 'vue'
+  import TodoPreview from '@/components/TodoPreview.vue'
 
   defineProps<{
     todos: Todo[]
@@ -12,7 +13,8 @@
 
 <template>
   <section>
-    <h1>List</h1>
-    <pre>{{ todos }}</pre>
+    <ul>
+      <TodoPreview v-for="todo in todos" :todo="todo" />
+    </ul>
   </section>
 </template>
---
+++ src/components/TodoPreview.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import {Todo} from '@/types/todo'
+  import {onMounted} from 'vue'
+
+  defineProps<{
+    todo: Todo
+  }>()
+  onMounted(() => {
+    console.log('hi preview')
+  })
+</script>
+
+<template>
+  <li class="clean-list">
+    <pre>{{ todo }}</pre>
+  </li>
+</template>
