e8b67182fe5c9b6fddeee8b39c56497d91aa1a2e
shforeal <rivena14798@gmail.com>
2022-07-10 16:37:05 +0300

    feat: add logic

--- src/views/TodoEdit.vue
+++ src/views/TodoEdit.vue
@@ -2,29 +2,34 @@
   import {useTodoStore} from '@/stores/todo'
   import {computed} from '@vue/reactivity'
   import {onMounted} from 'vue'
-  import {useRoute} from 'vue-router'
+  import {useRoute, useRouter} from 'vue-router'
   import {todoService} from '@/services/todo.service.js'
-  import {ref} from 'vue'
+  import {ref, reactive} from 'vue'
 
   const todoStore = useTodoStore()
   const route = useRoute()
-  // const todos = computed(() => todoStore.getTodos)
-  let todo = ref(null)
+  const router = useRouter()
+  const state = reactive({
+    todo: null,
+  })
   const id = route.params.id
   if (id) {
     todoStore.getTodoById(id)
-    todo.value = computed(() => todoStore.getCurrTodo)
-    console.log('todo', todo)
-  } else todo.value = computed(() => todoService.getEmptyTodo())
-  console.log('todo', todo)
+    state.todo = computed(() => todoStore.getCurrTodo)
+  } else state.todo = computed(() => todoService.getEmptyTodo())
+  function saveTodo() {
+    todoStore.addTodo(state.todo)
+    router.push('/todo')
+  }
 
-  // const todo = computed(() => )
   onMounted(() => {})
 </script>
 
 <template>
   <main class="container-layout-auto todo-app">
     <h1>Todo Edit</h1>
-    <pre>{{ todo }}</pre>
+    <pre>{{ state.todo }}</pre>
+    <input type="text" v-model="state.todo.txt" />
+    <button @click="saveTodo"></button>
   </main>
 </template>
